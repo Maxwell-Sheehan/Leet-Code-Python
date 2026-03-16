1fa3396711459bd8b22782504a5dbacf6b28847d
Maxwell-Sheehan <153469407+Maxwell-Sheehan@users.noreply.github.com>
2026-03-16 17:59:08 -0400

    Time: 23 ms (18.22%) | Memory: 19.2 MB (87.63%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -5,10 +5,9 @@ class Solution:
 
         counter: dict[str,int] = defaultdict(int)
         for r in range(len(s)):
-            counter[s[r]] +=1 # for each character at index r, we add it to our coutner dict
-            while counter[s[r]]  > 1:  # if the character added appears more then once
-                                    # we have aduplicate and need to remove it
-                counter[s[left]] -=1 # close left window to remove duplicate
-                left += 1
-            longest = max(longest, r - left + 1)
+            counter[s[r]] += 1
+            while counter[s[r]] > 1:
+                counter[s[left]] -= 1
+                left +=1
+            longest = max(longest, r - left +1)
         return longest
