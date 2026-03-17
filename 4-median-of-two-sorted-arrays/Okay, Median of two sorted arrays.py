d8e98a65bac6118799868abea49dc9fb46c46bff
Maxwell-Sheehan <153469407+Maxwell-Sheehan@users.noreply.github.com>
2026-03-16 20:03:55 -0400

    Add median finding solution for two sorted arrays
    Implement a solution to find the median of two sorted arrays using O((M+N)log(M+N)) complexity.

---
+++ 4-median-of-two-sorted-arrays/Okay, Median of two sorted arrays.py
@@ -0,0 +1,16 @@
+O((M+N)log(M+N)) Solution
+
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        array = sorted(nums1 + nums2)
+        length = len(array)
+
+        #check if even or odd for median
+        if length % 2 == 0:
+            return (array[length // 2 - 1] + array[length // 2]) /2
+        else: #it's even or odd so only two cases
+            # if odd return the middle
+            return array[length // 2]
+
+# this actually should fail, it's 
+# O((M+N)log(M+N))
