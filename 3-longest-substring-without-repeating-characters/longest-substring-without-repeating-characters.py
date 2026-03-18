38ddb5884f3ce21a73c980eb9ceb99faf472aa87
Maxwell-Sheehan <153469407+Maxwell-Sheehan@users.noreply.github.com>
2026-03-17 20:10:59 -0400

    Time: 11 ms (77.24%) | Memory: 19.3 MB (60.02%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -4,6 +4,7 @@ class Solution:
         longest = 0
 
         counter: dict[str,int] = defaultdict(int)
+
         for r in range(len(s)):
             counter[s[r]] += 1
             while counter[s[r]] > 1:
