3ffb281f88ba01fd9d63eb7273c0ad8a43c1639f
Maxwell-Sheehan <153469407+Maxwell-Sheehan@users.noreply.github.com>
2026-03-23 16:26:35 -0400

    Time: 16 ms (27.82%) | Memory: 19.3 MB (56.10%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -8,7 +8,7 @@ class Solution:
         for right in range(len(s)):
             counter[s[right]] += 1
             while counter[s[right]] > 1:
-                counter[s[left]] -=1 
-                left +=1
+                counter[s[left]] -=1
+                left += 1
             longest = max(longest, right - left + 1)
         return longest
