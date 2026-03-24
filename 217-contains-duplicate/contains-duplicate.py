9b1a0fcbe44a2d8ffbfb9ad10fd1eb1766aa600a
Maxwell-Sheehan <153469407+Maxwell-Sheehan@users.noreply.github.com>
2026-03-24 15:21:27 -0400

    Time: 11 ms (65.73%) | Memory: 32.3 MB (42.64%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -3,8 +3,7 @@ class Solution:
         seen = set()
 
         for num in nums:
-            if num in seen: # if num is in the set already, its not unique
+            if num in seen:
                 return True
-            seen.add(num) # add the num to the seen set
-
-        return False # if these checks fail, then it's false
+            seen.add(num)
+        return False
