43c9384edadd6f299c7fbb3d69e5be1a8d5ec320
Maxwell-Sheehan <153469407+Maxwell-Sheehan@users.noreply.github.com>
2026-03-24 13:38:59 -0400

    Time: 255 ms (62.54%) | Memory: 19.1 MB (96.22%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -6,15 +6,17 @@ class Solution:
             while left >= 0 and right < len(s) and s[left] == s[right]:
                 left -=1
                 right +=1
-            return s[left+1:right]
+            return s[left+1: right]
+
 
         for i in range(len(s)):
             p1 = expand(i,i)
-            p2 = expand(i, i + 1)
+
+            p2 = expand(i, i+1)
 
             if len(p1) > len(best):
                 best = p1
+
             if len(p2) > len(best):
                 best = p2
-
         return best
