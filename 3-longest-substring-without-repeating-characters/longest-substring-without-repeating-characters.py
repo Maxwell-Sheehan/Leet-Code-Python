16f283eab091c6b41bc8817c36e8f34433cdb74b
Maxwell-Sheehan <153469407+Maxwell-Sheehan@users.noreply.github.com>
2026-03-24 08:57:41 -0400

    Time: 11 ms (77.21%) | Memory: 19.4 MB (56.10%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -3,12 +3,12 @@ class Solution:
         left = 0
         longest = 0
 
-        counter: dict[str,int] = defaultdict(int)
+        counter: dict[str, int] = defaultdict(int)
 
         for right in range(len(s)):
             counter[s[right]] += 1
             while counter[s[right]] > 1:
-                counter[s[left]] -=1
+                counter[s[left]] -= 1
                 left += 1
-            longest = max(longest, right - left + 1)
+            longest = max(longest, right - left +1)
         return longest
