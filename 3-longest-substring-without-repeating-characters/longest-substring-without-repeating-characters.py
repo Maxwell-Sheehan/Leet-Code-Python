5362af6f7610c00fbdaabe9fb1370b5f2c16b674
Maxwell-Sheehan <153469407+Maxwell-Sheehan@users.noreply.github.com>
2026-03-25 08:30:55 -0400

    Time: 19 ms (23.18%) | Memory: 19.3 MB (84.26%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -3,7 +3,7 @@ class Solution:
         left = 0
         longest = 0
 
-        counter: dict[str,int] = defaultdict(int)
+        counter: dict[str:int] = defaultdict(int)
 
         for right in range(len(s)):
             counter[s[right]] += 1
@@ -11,5 +11,4 @@ class Solution:
                 counter[s[left]] -= 1
                 left += 1
             longest = max(longest, right - left + 1)
-
         return longest
