1a45a91747761c41eca8910e5cd4f3a37ea2fcfc
Maxwell-Sheehan <153469407+Maxwell-Sheehan@users.noreply.github.com>
2026-04-07 10:11:04 -0400

    Time: 0 ms (100.00%) | Memory: 19.4 MB (18.70%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -9,16 +9,20 @@ class Solution:
         tail = head
 
         while list1 and list2:
-            
+
             if list1.val < list2.val:
                 head.next = list1
                 list1 = list1.next
+            
             else:
                 head.next = list2
                 list2 = list2.next
             head = head.next
 
+        head.next = list1 if list1 else list2
+           
+            
 
-        head.next= list1 if list1 else list2
         return tail.next
 
+        
