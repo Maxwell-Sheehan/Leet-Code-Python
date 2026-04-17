ab1b09068223ae3f8c4cb62c1b0183d890f0e18f
Maxwell-Sheehan <153469407+Maxwell-Sheehan@users.noreply.github.com>
2026-04-17 10:01:03 -0400

    Time: 3 ms (34.11%) | Memory: 19.4 MB (36.53%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,7 +1,7 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
-        map = {")":"(", "]":"[", "}":"{"}
+        map = {"}":"{", ")":"(", "]":"["}
 
         for char in s:
             if char in map.values():
@@ -9,4 +9,5 @@ class Solution:
             elif char in map.keys():
                 if not stack or stack.pop() != map[char]:
                     return False
-        return not stack
+            
+        return not stack 
