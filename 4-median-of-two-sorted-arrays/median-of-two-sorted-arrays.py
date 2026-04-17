56ac6041b6510bb1da89e3556c0203521a2210f2
Maxwell-Sheehan <153469407+Maxwell-Sheehan@users.noreply.github.com>
2026-04-17 11:25:40 -0400

    Time: 5 ms (18.58%) | Memory: 19.7 MB (7.37%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
@@ -11,7 +11,7 @@ class Solution:
 
         while left <= right:
             part1 = (left + right) // 2
-            part2 = (len1 + len2 + 1) //2 - part1
+            part2 = (len1 + len2 + 1) // 2 - part1
 
             max_left1 = float('-inf') if part1 == 0 else nums1[part1 - 1]
             min_right1 = float('inf') if part1 == len1 else nums1[part1]
@@ -21,14 +21,13 @@ class Solution:
 
             if max_left1 <= min_right2 and max_left2 <= min_right1:
                 if (len1 + len2) % 2 == 0:
-                    return (max(max_left1, max_left2) + min(min_right1, min_right2)) /2 
+                    return (max(max_left1, max_left2) + min(min_right1, min_right2)) /2
                 else:
                     return max(max_left1, max_left2)
-                
             
             elif max_left1 > min_right2:
 
-                right = part1 - 1
+                right = part1 -1
 
             else:
                 left = part1 + 1
