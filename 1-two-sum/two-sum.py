0d84dfd83023aeab62d4db314b200c0fd9efca1e
Maxwell-Sheehan <153469407+Maxwell-Sheehan@users.noreply.github.com>
2026-04-20 09:38:20 -0400

    Time: 4 ms (39.34%) | Memory: 20.5 MB (23.49%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -4,8 +4,7 @@ class Solution:
 
         for i, num in enumerate(nums):
             compliment = target - num
-
             if compliment in seen:
                 return [seen[compliment], i]
-
+            
             seen[num] = i
