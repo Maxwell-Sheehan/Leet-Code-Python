f6d020eb2657ae7015ed7e4726ff4d8d6a045f92
Maxwell-Sheehan <153469407+Maxwell-Sheehan@users.noreply.github.com>
2026-04-20 16:26:03 -0400

    Time: 0 ms (100.00%) | Memory: 19.5 MB (48.59%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
@@ -3,6 +3,7 @@ class Solution:
         if len(nums1) > len(nums2):
             return self.findMedianSortedArrays(nums2, nums1)
 
+        
         len1 = len(nums1)
         len2 = len(nums2)
 
@@ -13,21 +14,23 @@ class Solution:
             part1 = (left + right) // 2
             part2 = (len1 + len2 + 1) // 2 - part1
 
-            max_left1 = float('-inf') if part1 == 0 else nums1[part1 - 1]
-            min_right1 = float('inf') if part1 == len1 else nums1[part1]
+            max_left1 = float("-inf") if part1 == 0 else nums1[part1 - 1]
+            min_right1 = float("inf") if part1 == len1 else nums1[part1]
 
-            max_left2 = float('-inf') if part2 == 0 else nums2[part2 - 1]
-            min_right2 = float('inf') if part2 == len2 else nums2[part2]
+            max_left2 = float("-inf") if part2 == 0 else nums2[part2 - 1]
+            min_right2 = float("inf") if part2 == len2 else nums2[part2]
 
             if max_left1 <= min_right2 and max_left2 <= min_right1:
                 if (len1 + len2) % 2 == 0:
-                    return (max(max_left1, max_left2) + min(min_right1, min_right2)) /2
+                    return (max(max_left1, max_left2) + min(min_right1, min_right2)) / 2
                 else:
                     return max(max_left1, max_left2)
+
             
             elif max_left1 > min_right2:
 
-                right = part1 -1
+                right = part1 - 1
 
             else:
                 left = part1 + 1
+
