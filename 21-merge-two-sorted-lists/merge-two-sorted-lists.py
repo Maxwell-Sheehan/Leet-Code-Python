9a259ccea8920210d4b3137cc8113fe765137a2d
Maxwell-Sheehan <153469407+Maxwell-Sheehan@users.noreply.github.com>
2026-04-27 10:42:26 -0400

    Time: 0 ms (100.00%) | Memory: 19.1 MB (99.39%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -12,10 +12,11 @@ class Solution:
             if list1.val < list2.val:
                 head.next = list1
                 list1 = list1.next
+
             else:
                 head.next = list2
                 list2 = list2.next
-            
+
             head = head.next
         
         head.next = list1 if list1 else list2
