4ec605c5a302100a1635f9428d8fcd8c939d52e8
Maxwell-Sheehan <153469407+Maxwell-Sheehan@users.noreply.github.com>
2026-04-27 12:28:59 -0400

    Time: 0 ms (100.00%) | Memory: 19.3 MB (78.93%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -4,18 +4,19 @@ class Solution:
 
         while left <= right:
             m = (left + right) // 2
+
             if nums[m] == target:
                 return m
-
+            
             elif nums[left] <= nums[m]:
                 if nums[left] <= target <= nums[m]:
-                    right = m -1
+                    right = m - 1
                 else:
                     left = m + 1
-            
             else:
                 if nums[m] <= target <= nums[right]:
                     left = m + 1
                 else:
-                    right = m-1
-        return -1 
+                    right = m - 1
+            
+        return -1
