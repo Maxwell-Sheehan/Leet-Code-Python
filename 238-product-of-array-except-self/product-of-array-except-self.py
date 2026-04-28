49669a2962b434a592d8d74be1460ff469d85e24
Maxwell-Sheehan <153469407+Maxwell-Sheehan@users.noreply.github.com>
2026-04-28 13:31:43 -0400

    Time: 17 ms (83.31%) | Memory: 24.9 MB (96.75%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,6 +1,6 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        
+
         output = [1] * len(nums)
 
         left = 1
@@ -9,10 +9,10 @@ class Solution:
             output[i] *= left
             left *= nums[i]
 
-        right = 1
+            right = 1
 
         for i in range(len(nums) -1, -1, -1):
             output[i] *= right
             right *= nums[i]
-
+        
         return output
