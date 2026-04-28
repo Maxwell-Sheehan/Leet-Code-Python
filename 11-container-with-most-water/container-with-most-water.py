2486b4dcc4aa70ab362cb88229e22f8e0ea72e70
Maxwell-Sheehan <153469407+Maxwell-Sheehan@users.noreply.github.com>
2026-04-28 13:27:17 -0400

    Time: 59 ms (52.21%) | Memory: 29.5 MB (88.74%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -9,7 +9,9 @@ class Solution:
 
             if height[left] < height[right]:
                 left += 1
-
+            
             else:
                 right -= 1
+        
         return max_area
+
