aedb12cee1e744a719f52b5e4b6f2d4f3760c042
Maxwell-Sheehan <153469407+Maxwell-Sheehan@users.noreply.github.com>
2026-04-28 09:10:02 -0400

    Time: 627 ms (62.55%) | Memory: 22.2 MB (54.88%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -4,21 +4,20 @@ class Solution:
         nums.sort()
 
         for i in range(len(nums)):
-
             if i > 0 and nums[i] == nums[i-1]:
                 continue
-
+        
             j = i + 1
             k = len(nums) - 1
 
             while j < k:
                 total = nums[i] + nums[j] + nums[k]
 
-                if total > 0:
-                    k -= 1
-                elif total < 0:
+                if total < 0:
                     j += 1
-                
+                elif total > 0:
+                    k -= 1
+
                 else:
                     res.append([nums[i], nums[j], nums[k]])
 
@@ -26,4 +25,5 @@ class Solution:
 
                     while nums[j] == nums[j-1] and j < k:
                         j += 1
+
         return res
