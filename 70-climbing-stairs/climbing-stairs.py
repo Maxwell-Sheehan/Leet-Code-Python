d0a12bb6899c9c0165a112688e5964b9ef374807
Maxwell-Sheehan <153469407+Maxwell-Sheehan@users.noreply.github.com>
2026-05-13 13:22:01 -0400

    Time: 0 ms (100.00%) | Memory: 19.3 MB (52.00%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -7,9 +7,9 @@ class Solution:
         prev_step = 3
         prev_step2 = 2
 
-        for i in range(3, n):
+        for i in range(3,n):
             curr = prev_step + prev_step2
             prev_step2 = prev_step
             prev_step = curr
-
+        
         return curr
