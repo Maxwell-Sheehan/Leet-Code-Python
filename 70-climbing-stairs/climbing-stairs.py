2bf2893aefc5f74d71c58f7671f37abcf3ce1a88
Maxwell-Sheehan <153469407+Maxwell-Sheehan@users.noreply.github.com>
2026-05-18 16:59:42 -0400

    Time: 0 ms (100.00%) | Memory: 19.2 MB (86.02%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -2,14 +2,13 @@ class Solution:
     def climbStairs(self, n: int) -> int:
         if n <= 3:
             return n
-
+        
         curr = 0
         prev_step = 3
         prev_step2 = 2
 
-        for i in range(3,n):
+        for i in range(3, n):
             curr = prev_step + prev_step2
             prev_step2 = prev_step
             prev_step = curr
-        
         return curr
